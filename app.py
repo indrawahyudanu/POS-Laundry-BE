e31369833a2b4e4ccecf6c0a714fe2985020cd8a
Indra Wahyu <indrawahyudanu@gmail.com>
2026-09-11 15:40:36 +0700

    Add configuration file and register authentication blueprint

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask,jsonify
+from routes.auth_routes import auth_bp
 
 app = Flask(__name__)
+
+app.register_blueprint(auth_bp, url_prefix='/api/auth')
 
 @app.route('/')
 def home():
